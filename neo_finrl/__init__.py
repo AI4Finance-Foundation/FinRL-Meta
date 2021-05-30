16f6f2afa6033e9e15010ab690146baf1267ad9f
rayrui312 <61444295+rayrui312@users.noreply.github.com>
2021-05-30 08:28:08 +0800

    Update __init__.py

--- neo_finrl/__init__.py
+++ neo_finrl/__init__.py
@@ -1,11 +1,1 @@
-from gym.envs.registration import register
 
-register(
-    id='tradingEnv-v0',
-    entry_point='gym_finrl.envs:StockTradingEnv',
-)
-
-# register(
-#     id='trading_env_v1',
-#     entry_point='gym_finrl.envs:StockTradingEnv',
-# )
