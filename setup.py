3b9367004d4507b1b88a04111e153189b316a496
rayrui312 <61444295+rayrui312@users.noreply.github.com>
2021-05-30 08:35:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,5 @@
 from setuptools import setup, find_packages
-NAME = "neofinrl"
-PACKAGES = [NAME] + ["%s.%s" % (NAME, i) for i in find_packages(NAME)]
 setup(name='neofinrl',
       version='0.0.1',
-      packages=PACKAGES,
+      packages=find_packages(),
       install_requires=['gym', 'numpy'])
