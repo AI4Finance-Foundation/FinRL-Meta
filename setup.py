7b348c0e561ab8235378af81c741768f2feed2e4
rayrui312 <61444295+rayrui312@users.noreply.github.com>
2021-05-30 08:22:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-setup(name='neofinrl',
+setup(name='gym_finrl',
       version='0.0.1',
       packages=find_packages(),
       install_requires=['gym', 'numpy'])
