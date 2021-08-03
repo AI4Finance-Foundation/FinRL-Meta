601045a1ff8ca6ee7f9653358c7970d6879c8d24
rayrui312 <61444295+rayrui312@users.noreply.github.com>
2021-08-04 01:13:03 +0800

    Update test.py

--- test.py
+++ test.py
@@ -75,6 +75,68 @@ def test(data_dic, drl_lib, env, agent, **kwargs):
         #return episode returns on testing data
         return episode_returns
     
+    #test using rllib
+    elif drl_lib == 'rllib':
+        #load agent
+        from ray.rllib.agents import ppo
+        from ray.rllib.agents.ppo.ppo import PPOTrainer
+        config = ppo.DEFAULT_CONFIG.copy()
+        config['env'] = env
+        config["log_level"] = "WARN"
+        config['env_config'] = {'price_ary':price_ary,
+                                'tech_ary':tech_ary,
+                                'turbulence_ary':turbulence_ary,
+                                'if_train':False}
+        
+        trainer = PPOTrainer(env=env, config=config)
+        try:
+            trainer.restore(cwd)
+            print("Restoring from checkpoint path", cwd)
+        except:
+            raise ValueError('Fail to load agent!')
+        
+        #test on the testing env
+        state = env_instance.reset()
+        episode_returns = list()  # the cumulative_return / initial_account
+        done = False
+        while not done:
+            action = trainer.compute_single_action(state)
+            state, reward, done, _ = env_instance.step(action)
+    
+            total_asset = env_instance.amount + (env_instance.price_ary[env_instance.day] * env_instance.stocks).sum()
+            episode_return = total_asset / env_instance.initial_total_asset
+            episode_returns.append(episode_return)
+        print('episode return: ' + str(episode_return))
+        print('Test Finished!')   
+        return episode_returns
+
+          #test using stable baselines3
+    elif drl_lib == 'stable_baselines3':
+        from stable_baselines3 import PPO
+
+        try:
+            #load agent
+            model = PPO.load(cwd)
+            print("Successfully load model", cwd)
+        except:
+            raise ValueError('Fail to load agent!')
+        
+        #test on the testing env
+        state = env_instance.reset()
+        episode_returns = list()  # the cumulative_return / initial_account
+        done = False
+        while not done:
+            action = model.predict(state)[0]
+            state, reward, done, _ = env_instance.step(action)
+    
+            total_asset = env_instance.amount + (env_instance.price_ary[env_instance.day] * env_instance.stocks).sum()
+            episode_return = total_asset / env_instance.initial_total_asset
+            episode_returns.append(episode_return)
+        
+        print('episode_return', episode_return)
+        print('Test Finished!')   
+        return episode_returns
+    
     else:
         raise ValueError('DRL library input is NOT supported yet. Please check.')
             
@@ -110,3 +172,11 @@ if __name__ == '__main__':
     #demo for elegantrl
     test(data_dic, drl_lib='elegantrl', env=env, agent='ppo', 
     cwd='./test_ppo', net_dimension = 2 ** 9)
+    
+    #demo for rllib 
+    test(data_dic, drl_lib='rllib', env=env, agent='ppo', 
+    cwd='./test_ppo/checkpoint_000010/checkpoint-10')
+
+    #demo for stable baselines3 
+    test(data_dic, drl_lib='stable_baselines3', env=env, agent='ppo', 
+    cwd='./test_ppo.zip')
