0e30f1de6e4ee5394a322a25e1c821806fecec90
rayrui312 <61444295+rayrui312@users.noreply.github.com>
2021-08-05 01:18:59 +0800

    Update trade.py

--- trade.py
+++ trade.py
@@ -2,77 +2,149 @@ from elegantrl.agent import *
 from elegantrl.run import *
 import torch 
 
-
-def trade(data_dic, drl_lib, env, agent, cwd, net_dimension):
-    if 'price_ary' in data_dic and 'tech_ary' in data_dic and 'turbulence_ary'\
-    in data_dic:
-        price_ary = data_dic['price_ary']
-        tech_ary = data_dic['tech_ary']
-        turbulence_ary = data_dic['turbulence_ary']
-    elif 'price_ary' in data_dic and 'tech_ary' in data_dic and 'turbulence_ary'\
-    not in data_dic:
-        price_ary = data_dic['price_ary']
-        tech_ary = data_dic['tech_ary']
-    else:
-        raise ValueError('Invalid input data_dic!')
+def trade(data_dic, drl_lib, env, agent, **kwargs, mode='backtesting'):
     
-    env_config = {'price_ary':price_ary,
-            'tech_ary':tech_ary,
-            'turbulence_ary':turbulence_ary,
-            'if_train':False}
-    env_instance = env(config=env_config)
-
-    #trade using elegantrl
-    if drl_lib == 'elegantrl':
-        
-        #select agent
-        if agent == 'ppo':
-            args = Arguments(agent=AgentPPO(), env=env_instance, if_on_policy=True)
+    if mode == 'backtesting':
+        if 'price_ary' in data_dic and 'tech_ary' in data_dic and 'turbulence_ary'\
+        in data_dic:
+            price_ary = data_dic['price_ary']
+            tech_ary = data_dic['tech_ary']
+            turbulence_ary = data_dic['turbulence_ary']
+        elif 'price_ary' in data_dic and 'tech_ary' in data_dic and 'turbulence_ary'\
+        not in data_dic:
+            price_ary = data_dic['price_ary']
+            tech_ary = data_dic['tech_ary']
         else:
-            raise ValueError('Invalid agent input or the agent input is not \
-                             supported yet.')
-        
-        #load agent
-        try:
-            state_dim = env.state_dim
-            action_dim = env.action_dim
-    
-            agent = args.agent
-            net_dim = net_dimension
-    
-            agent.init(net_dim, state_dim, action_dim)
-            agent.save_load_model(cwd=cwd, if_save=False)
-            act = agent.act
-            device = agent.device
-    
-        except:
-            raise ValueError('Fail to load agent!')
-        
-        #trade on the trading env
-        state = env.reset()
-        episode_returns = list()  # the cumulative_return / initial_account
-        with _torch.no_grad():
-            for i in range(env.max_step):
-                s_tensor = _torch.as_tensor((state,), device=device)
-                a_tensor = act(s_tensor)  # action_tanh = act.forward()
-                action = a_tensor.detach().cpu().numpy()[0]  # not need detach(), because with torch.no_grad() outside
-                state, reward, done, _ = env.step(action)
+            raise ValueError('Invalid input data_dic!')
 
-                total_asset = env.amount + (env.price_ary[env.day] * env.stocks).sum()
-                episode_return = total_asset / env.initial_total_asset
+        env_config = {'price_ary':price_ary,
+                'tech_ary':tech_ary,
+                'turbulence_ary':turbulence_ary,
+                'if_train':False}
+        env_instance = env(config=env_config)
+        net_dimension = kwargs.get('net_dimension', 2**7)
+        cwd = kwargs.get('cwd','./'+str(agent))
+
+        #trade using elegantrl
+        if drl_lib == 'elegantrl':
+
+            #select agent
+            if agent == 'ppo':
+                args = Arguments(agent=AgentPPO(), env=env_instance, if_on_policy=True)
+            else:
+                raise ValueError('Invalid agent input or the agent input is not \
+                                 supported yet.')
+
+            #load agent
+            try:
+                state_dim = env_instance.state_dim
+                action_dim = env_instance.action_dim
+
+                agent = args.agent
+                net_dim = net_dimension
+
+                agent.init(net_dim, state_dim, action_dim)
+                agent.save_load_model(cwd=cwd, if_save=False)
+                act = agent.act
+                device = agent.device
+
+            except:
+                raise ValueError('Fail to load agent!')
+
+            #trade on the trading env
+            _torch = torch
+            state = env_instance.reset()
+            episode_returns = list()  # the cumulative_return / initial_account
+            with _torch.no_grad():
+                for i in range(env_instance.max_step):
+                    s_tensor = _torch.as_tensor((state,), device=device)
+                    a_tensor = act(s_tensor)  # action_tanh = act.forward()
+                    action = a_tensor.detach().cpu().numpy()[0]  # not need detach(), because with torch.no_grad() outside
+                    state, reward, done, _ = env_instance.step(action)
+
+                    total_asset = env_instance.amount + (env_instance.price_ary[env_instance.day] * env_instance.stocks).sum()
+                    episode_return = total_asset / env_instance.initial_total_asset
+                    episode_returns.append(episode_return)
+                    if done:
+                        break
+            print('Test Finished!')
+            #return episode returns on testing data
+            return episode_returns
+
+        #trade using rllib
+        elif drl_lib == 'rllib':
+            #load agent
+            from ray.rllib.agents import ppo
+            from ray.rllib.agents.ppo.ppo import PPOTrainer
+            config = ppo.DEFAULT_CONFIG.copy()
+            config['env'] = env
+            config["log_level"] = "WARN"
+            config['env_config'] = {'price_ary':price_ary,
+                                    'tech_ary':tech_ary,
+                                    'turbulence_ary':turbulence_ary,
+                                    'if_train':False}
+
+            trainer = PPOTrainer(env=env, config=config)
+            try:
+                trainer.restore(cwd)
+                print("Restoring from checkpoint path", cwd)
+            except:
+                raise ValueError('Fail to load agent!')
+
+            #trade on the trading env
+            state = env_instance.reset()
+            episode_returns = list()  # the cumulative_return / initial_account
+            done = False
+            while not done:
+                action = trainer.compute_single_action(state)
+                state, reward, done, _ = env_instance.step(action)
+
+                total_asset = env_instance.amount + (env_instance.price_ary[env_instance.day] * env_instance.stocks).sum()
+                episode_return = total_asset / env_instance.initial_total_asset
                 episode_returns.append(episode_return)
-                if done:
-                    break
+            print('episode return: ' + str(episode_return))
+            print('Test Finished!')   
+            return episode_returns
+
+        #trade using stable baselines3
+        elif drl_lib == 'stable_baselines3':
+            from stable_baselines3 import PPO
+
+            try:
+                #load agent
+                model = PPO.load(cwd)
+                print("Successfully load model", cwd)
+            except:
+                raise ValueError('Fail to load agent!')
+
+            #trade on the trading env
+            state = env_instance.reset()
+            episode_returns = list()  # the cumulative_return / initial_account
+            done = False
+            while not done:
+                action = model.predict(state)[0]
+                state, reward, done, _ = env_instance.step(action)
+
+                total_asset = env_instance.amount + (env_instance.price_ary[env_instance.day] * env_instance.stocks).sum()
+                episode_return = total_asset / env_instance.initial_total_asset
+                episode_returns.append(episode_return)
+
+            print('episode_return', episode_return)
+            print('Test Finished!')   
+            return episode_returns
+
+        else:
+            raise ValueError('DRL library input is NOT supported yet. Please check.')
+
+    elif mode == 'paper_trading':
+        print('Paper trading is NOT supported for now.')
         
-        #return episode returns on trading data
-        return episode_returns
-    
     else:
-        raise ValueError('DRL library input is NOT supported yet. Please check.')
+        raise ValueError("Invalid mode input! Please input either 'backtesting' or 'paper_trading'.")
             
 if __name__ == '__main__':    
     #fetch data
-    from neo_finrl.data_processors.alpaca_engineer import AlpacaEngineer as AE
+    from neo_finrl.data_processors.processor_alpaca import AlpacaEngineer as AE
     API_KEY = ""
     API_SECRET = ""
     APCA_API_BASE_URL = 'https://paper-api.alpaca.markets'
@@ -96,10 +168,19 @@ if __name__ == '__main__':
     data_dic = {'price_ary':price_ary, 'tech_ary':tech_ary, 'turbulence_ary':turb_ary}
     
     #construct environment
-    from neo_finrl.environments.env_stock_trading.env_stock_alpaca import StockTradingEnv
+    from neo_finrl.env_stock_trading.env_stock_alpaca import StockTradingEnv
     env = StockTradingEnv
     
     #demo for elegantrl
-    trade(data_dic, drl_lib='elegantrl', env=env, agent='ppo', cwd='./test_ppo_erl',
-         net_dimension = 2 ** 7)
+    test(data_dic, drl_lib='elegantrl', env=env, agent='ppo', 
+    cwd='./test_ppo', net_dimension = 2 ** 9, mode='backtesting')
+    
+    #demo for rllib 
+    test(data_dic, drl_lib='rllib', env=env, agent='ppo', 
+    cwd='./test_ppo/checkpoint_000010/checkpoint-10', mode='backtesting')
+
+    #demo for stable baselines3 
+    test(data_dic, drl_lib='stable_baselines3', env=env, agent='ppo', 
+    cwd='./test_ppo.zip', mode='backtesting')
+
 
