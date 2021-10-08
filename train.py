6089de42ab143e31a69e7d436a5bb1c5558146c9
rayrui312 <61444295+rayrui312@users.noreply.github.com>
2021-10-08 20:52:13 +0800

    Update train.py
    Support newest ElegantRL (2021/10/08).
    Support tuning hyper-parameters of PPO algorithm of RLlib

--- train.py
+++ train.py
@@ -4,10 +4,11 @@ import torch
 import ray
 from neo_finrl.data_processor import DataProcessor
 
-def train(start_date, end_date, ticker_list, data_source, time_interval,
-          technical_indicator_list, drl_lib, env, agent, if_vix=True,
+def train(start_date, end_date, ticker_list, data_source, time_interval, 
+          technical_indicator_list, drl_lib, env, agent, if_vix = True,
           **kwargs):
-    # fetch data
+    
+    #fetch data
     DP = DataProcessor(data_source, **kwargs)
     data = DP.download_data(ticker_list, start_date, end_date, time_interval)
     data = DP.clean_data(data)
@@ -15,72 +16,54 @@ def train(start_date, end_date, ticker_list, data_source, time_interval,
     if if_vix:
         data = DP.add_vix(data)
     price_array, tech_array, risk_array = DP.df_to_array(data, if_vix)
-
-    # read parameters
-    env_config = {'price_array': price_array,
-                  'tech_array': tech_array,
-                  'risk_array': risk_array,
-                  'if_train': True}
+    
+    #read parameters
+    env_config = {'price_array':price_array,
+            'tech_array':tech_array,
+            'risk_array':risk_array,
+            'if_train':True}
     env_instance = env(config=env_config)
-
-    learning_rate = kwargs.get('learning_rate', 0.00025)
-    batch_size = kwargs.get('batch_size', 2 ** 9)
+    
+    learning_rate = kwargs.get('learning_rate', 2 ** -15)
+    batch_size = kwargs.get('batch_size', 2 ** 11 )
     gamma = kwargs.get('gamma', 0.99)
     seed = kwargs.get('seed', 312)
     total_timesteps = kwargs.get('total_timesteps', 1e6)
-    net_dimension = kwargs.get('net_dimension', 2 ** 7)
-    cwd = kwargs.get('cwd', './' + str(agent))
-
-    # train using different libraries
+    net_dimension = kwargs.get('net_dimension', 2**9)
+    cwd = kwargs.get('cwd','./'+str(agent))
+    
+    #train using different libraries
     if drl_lib == 'elegantrl':
-
+        
         if agent == 'ppo':
             args = Arguments(if_on_policy=True)
+            args.agent = AgentPPO()
+            args.env = env_instance
+            args.agent.if_use_cri_target = True
+            args.cwd = cwd
         else:
             raise ValueError('Invalid agent input or the agent input is not \
                              supported yet.')
-        # try:
-        #     args.agent = AgentPPO()
-        #     args.env=env_instance
-        #     args.cwd = cwd
-        #     args.learning_rate = learning_rate
-        #     args.batch_size = batch_size
-        #     args.gamma = gamma
-        #     args.seed = seed
-        #     args.break_step = total_timesteps
-        #     args.net_dimension = net_dimension
-        # except:
-        #     print('Invalid parameters input! Use default value.')
-        #     args.agent = AgentPPO()
-        #     args.env=env_instance
-        #     args.learning_rate = 0.00025
-        #     args.batch_size = 128
-        #     args.gamma = 0.99
-        #     args.seed = 312
-        #     args.break_step = 1e6
-        #     args.net_dimension = 2**7
-
-        args.cwd = cwd
-        args.env = env_instance
-
-        args.agent = AgentPPO()
-        args.agent.if_use_cri_target = True
-        args.learning_rate = learning_rate  
-        args.net_dim = net_dimension
-        args.batch_size = batch_size
-        args.gamma = gamma
-        args.seed = seed
-        args.break_step = int(total_timesteps)
-
+        try:
+            args.learning_rate = learning_rate
+            args.batch_size = batch_size
+            args.gamma = gamma
+            args.seed = seed
+            args.break_step = total_timesteps
+            args.net_dimension = net_dimension
+        except:
+            print('Invalid parameters input! Use default value.')
+            args.learning_rate = 2 ** -15
+            args.batch_size = 2 ** 11
+            args.gamma = 0.99
+            args.seed = 312
+            args.break_step = 1e6
+            args.net_dimension = 2**9
+            
         train_and_evaluate(args)
-        #args.worker_num = 4
-        #args.target_step = args.env.max_step * 2
-        #train_and_evaluate_mp(args)
-
+        
     elif drl_lib == 'rllib':
-        # ray.init(num_gpus=1, local_mode = True)
-        ray.init()
-        assert ray.is_initialized() == True
+        ray.init(ignore_reinit_error=True)
         if agent == 'ppo':
             from ray.rllib.agents import ppo
             from ray.rllib.agents.ppo.ppo import PPOTrainer
@@ -90,40 +73,41 @@ def train(start_date, end_date, ticker_list, data_source, time_interval,
                 print('total training episodes is not given! Use default value 1000')
                 total_episodes = 1000
             config = ppo.DEFAULT_CONFIG.copy()
+            config['lr'] = learning_rate
+            config['train_batch_size'] = batch_size
+            config['gamma'] = gamma
             config['env'] = env
-            config["log_level"] = "WARN"
-            config['gamma'] = 0.99
-            config['env_config'] = {'price_array': price_array,
-                                    'tech_array': tech_array,
-                                    'risk_array': risk_array,
-                                    'if_train': True}
+            config['log_level'] = "WARN"
+            config['env_config'] = {'price_array':price_array,
+                                    'tech_array':tech_array,
+                                    'turbulence_array':turbulence_array,
+                                    'if_train':True}
             trainer = PPOTrainer(env=env, config=config)
             for i in range(total_episodes):
                 trainer.train()
             trainer.save(cwd)
-
+        
         else:
             raise ValueError('Invalid agent input or the agent input is not \
                  supported yet.')
-
+            
     elif drl_lib == 'stable_baselines3':
-
+        
         if agent == 'ppo':
             from stable_baselines3 import PPO
             from stable_baselines3.common.vec_env import DummyVecEnv
-
-            env_train = DummyVecEnv([lambda: env_instance])
-            model = PPO("MlpPolicy", env_train, learning_rate=learning_rate,
-                        n_steps=2048, batch_size=batch_size, ent_coef=0.0,
+            
+            env_train = DummyVecEnv([lambda : env_instance])
+            model = PPO("MlpPolicy", env_train, learning_rate=learning_rate, 
+                        n_steps=2048, batch_size=batch_size, ent_coef=0.0, 
                         gamma=gamma, seed=seed)
-            model.learn(total_timesteps=total_timesteps, tb_log_name='ppo')
+            model.learn(total_timesteps=total_timesteps, tb_log_name = 'ppo')
             print('Training finished!')
             model.save(cwd)
             print('Trained model saved in ' + str(cwd))
-
+    
     else:
         raise ValueError('DRL library input is NOT supported. Please check.')
-
 
 if __name__ == '__main__':
     from neo_finrl.config import FAANG_TICKER
