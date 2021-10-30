a02c621bc28b6b14eef07c9a98aa6c1a1e553e5e
rayrui312 <rayrui@connect.hku.hk>
2021-10-30 11:45:47 +0800

    2021/10/30 update
    Delete elegantrl, rllib, stable-baselines3, gpupodracer folders.

--- rllib/execution/multi_gpu_impl.py
+++
@@ -1,5 +0,0 @@
-from ray.rllib.policy.dynamic_tf_policy import TFMultiGPUTowerStack
-from ray.rllib.utils.deprecation import deprecation_warning
-
-deprecation_warning("LocalSyncParallelOptimizer", "TFMultiGPUTowerStack")
-LocalSyncParallelOptimizer = TFMultiGPUTowerStack
--- stable_baselines3/her/__init__.py
+++
@@ -1,2 +0,0 @@
-from stable_baselines3.her.goal_selection_strategy import GoalSelectionStrategy
-from stable_baselines3.her.her_replay_buffer import HerReplayBuffer
