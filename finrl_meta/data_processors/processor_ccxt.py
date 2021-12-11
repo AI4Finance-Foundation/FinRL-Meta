c755ecff27f028e88968b34cd3c3cbc8da00b219
mingzhu <zhumingpassional@gmail.com>
2021-12-11 17:51:49 +0800

    revise init function

--- finrl_meta/data_processors/processor_ccxt.py
+++ finrl_meta/data_processors/processor_ccxt.py
@@ -8,6 +8,7 @@ from stockstats import StockDataFrame as Sdf
 # from basic_processor import BasicProcessor
 from finrl_meta.data_processors.basic_processor import BasicProcessor
 
+
 class CCXTProcessor(BasicProcessor):
     def __init__(self, data_source: str, **kwargs):
         BasicProcessor.__init__(self, data_source, **kwargs)
