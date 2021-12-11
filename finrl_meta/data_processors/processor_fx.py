c56040ed9f36321dbb8fe7e1955f15fdd2904690
mingzhu <zhumingpassional@gmail.com>
2021-12-11 17:22:53 +0800

    init processor based on basic class

--- finrl_meta/data_processors/processor_fx.py
+++ finrl_meta/data_processors/processor_fx.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pandas as pd
 from finta import TA
-from basic_processor import BasicProcessor
+from finrl_meta.data_processors.basic_processor import BasicProcessor
 
 def add_time_feature(df,symbol, dt_col_name = 'time'):
     """read csv into df and index on time
