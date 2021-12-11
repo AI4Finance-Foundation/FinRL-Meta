7a92f9ff74dd18a8be98f773e6150bc57fb929fc
mingzhu <zhumingpassional@gmail.com>
2021-12-11 17:51:58 +0800

    revise init function

--- finrl_meta/data_processors/processor_quantconnect.py
+++ finrl_meta/data_processors/processor_quantconnect.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 # from basic_processor import BasicProcessor
 from finrl_meta.data_processors.basic_processor import BasicProcessor
+
 class QuantConnectEngineer(BasicProcessor):
     def __init__(self, data_source: str, **kwargs):
         BasicProcessor.__init__(self, data_source, **kwargs)
