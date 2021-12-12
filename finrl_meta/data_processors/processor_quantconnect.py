8f195efcf6deb028ef0f0b5b94a52e7ad599fd2f
mingzhu <zhumingpassional@gmail.com>
2021-12-12 16:12:16 +0800

    revise quantconnect

--- finrl_meta/data_processors/processor_quantconnect.py
+++ finrl_meta/data_processors/processor_quantconnect.py
@@ -40,14 +40,14 @@ class QuantConnectEngineer(BasicProcessor):
         return history
 
 
-    def preprocess(df, stock_list):
-        df = df[['open','high','low','close','volume']]
-        if_first_time = True
-        for stock in stock_list:
-            if if_first_time:
-                ary = df.loc[stock].values
-                if_first_time = False
-            else:
-                temp = df.loc[stock].values
-                ary = np.hstack((ary,temp))
-        return ary
+    # def preprocess(df, stock_list):
+    #     df = df[['open','high','low','close','volume']]
+    #     if_first_time = True
+    #     for stock in stock_list:
+    #         if if_first_time:
+    #             ary = df.loc[stock].values
+    #             if_first_time = False
+    #         else:
+    #             temp = df.loc[stock].values
+    #             ary = np.hstack((ary,temp))
+    #     return ary
