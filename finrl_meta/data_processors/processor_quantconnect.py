ff29fd833cbab535effb9a19afbc35157d6d6efc
mingzhu <zhumingpassional@gmail.com>
2021-12-12 16:11:44 +0800

    revise quantconnect

--- finrl_meta/data_processors/processor_quantconnect.py
+++ finrl_meta/data_processors/processor_quantconnect.py
@@ -17,13 +17,13 @@ class QuantConnectEngineer(BasicProcessor):
     def __init__(self, data_source: str, **kwargs):
         BasicProcessor.__init__(self, data_source, **kwargs)
 
-    def data_fetch(start_time, end_time, stock_list, resolution=Resolution.Daily) :
-        #resolution: Daily, Hour, Minute, Second
-        qb = QuantBook()
-        for stock in stock_list:
-            qb.AddEquity(stock)
-        history = qb.History(qb.Securities.Keys, start_time, end_time, resolution)
-        return history
+    # def data_fetch(start_time, end_time, stock_list, resolution=Resolution.Daily) :
+    #     #resolution: Daily, Hour, Minute, Second
+    #     qb = QuantBook()
+    #     for stock in stock_list:
+    #         qb.AddEquity(stock)
+    #     history = qb.History(qb.Securities.Keys, start_time, end_time, resolution)
+    #     return history
 
     def download_data(self, ticker_list: List[str], start_date: str, end_date: str, time_interval: str) -> pd.DataFrame:
         self.start = start_date
