e5f4f5050a9d2b82e9aba0feaf7c751ba671a0d6
Jingyang Rui (Ray) <61444295+rayrui312@users.noreply.github.com>
2021-12-25 15:20:33 +0800

    Update processor_binance.py

--- finrl_meta/data_processors/processor_binance.py
+++ finrl_meta/data_processors/processor_binance.py
@@ -3,8 +3,8 @@ from typing import List
 import numpy as np
 import pandas as pd
 import requests
-# from talib.abstract import CCI, DX, MACD, RSI
-# from basic_processor import BasicProcessor
+from talib.abstract import CCI, DX, MACD, RSI
+from basic_processor import BasicProcessor
 from finrl_meta.data_processors.basic_processor import BasicProcessor
 
 
