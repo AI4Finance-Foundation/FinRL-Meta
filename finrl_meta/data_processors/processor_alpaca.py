6898b33aa3f3924970da4ffa8fe554c31bdfb520
Ming Zhu <zhumingpassional@gmail.com>
2022-01-12 21:53:49 +0800

    revise add_turbulence

--- finrl_meta/data_processors/processor_alpaca.py
+++ finrl_meta/data_processors/processor_alpaca.py
@@ -187,65 +187,65 @@ class AlpacaProcessor(BasicProcessor):
     #     df = df.sort_values(["time", "tic"]).reset_index(drop=True)
     #     return df
 
-    def calculate_turbulence(self, data, time_period=252):
-        # can add other market assets
-        df = data.copy()
-        df_price_pivot = df.pivot(index="date", columns="tic", values="close")
-        # use returns to calculate turbulence
-        df_price_pivot = df_price_pivot.pct_change()
-
-        unique_date = df.date.unique()
-        # start after a fixed time period
-        start = time_period
-        turbulence_index = [0] * start
-        # turbulence_index = [0]
-        count = 0
-        for i in range(start, len(unique_date)):
-            current_price = df_price_pivot[df_price_pivot.index == unique_date[i]]
-            # use one year rolling window to calcualte covariance
-            hist_price = df_price_pivot[
-                (df_price_pivot.index < unique_date[i])
-                & (df_price_pivot.index >= unique_date[i - time_period])
-            ]
-            # Drop tickers which has number missing values more than the "oldest" ticker
-            filtered_hist_price = hist_price.iloc[
-                hist_price.isna().sum().min() :
-            ].dropna(axis=1)
-
-            cov_temp = filtered_hist_price.cov()
-            current_temp = current_price[[x for x in filtered_hist_price]] - np.mean(
-                filtered_hist_price, axis=0
-            )
-            temp = current_temp.values.dot(np.linalg.pinv(cov_temp)).dot(
-                current_temp.values.T
-            )
-            if temp > 0:
-                count += 1
-                if count > 2:
-                    turbulence_temp = temp[0][0]
-                else:
-                    # avoid large outlier because of the calculation just begins
-                    turbulence_temp = 0
-            else:
-                turbulence_temp = 0
-            turbulence_index.append(turbulence_temp)
-
-        turbulence_index = pd.DataFrame(
-            {"date": df_price_pivot.index, "turbulence": turbulence_index}
-        )
-        return turbulence_index
-
-    def add_turbulence(self, data, time_period=252):
-        """
-        add turbulence index from a precalcualted dataframe
-        :param data: (df) pandas dataframe
-        :return: (df) pandas dataframe
-        """
-        df = data.copy()
-        turbulence_index = self.calculate_turbulence(df, time_period=time_period)
-        df = df.merge(turbulence_index, on="date")
-        df = df.sort_values(["date", "tic"]).reset_index(drop=True)
-        return df
+    # def calculate_turbulence(self, data, time_period=252):
+    #     # can add other market assets
+    #     df = data.copy()
+    #     df_price_pivot = df.pivot(index="date", columns="tic", values="close")
+    #     # use returns to calculate turbulence
+    #     df_price_pivot = df_price_pivot.pct_change()
+    #
+    #     unique_date = df.date.unique()
+    #     # start after a fixed time period
+    #     start = time_period
+    #     turbulence_index = [0] * start
+    #     # turbulence_index = [0]
+    #     count = 0
+    #     for i in range(start, len(unique_date)):
+    #         current_price = df_price_pivot[df_price_pivot.index == unique_date[i]]
+    #         # use one year rolling window to calcualte covariance
+    #         hist_price = df_price_pivot[
+    #             (df_price_pivot.index < unique_date[i])
+    #             & (df_price_pivot.index >= unique_date[i - time_period])
+    #         ]
+    #         # Drop tickers which has number missing values more than the "oldest" ticker
+    #         filtered_hist_price = hist_price.iloc[
+    #             hist_price.isna().sum().min() :
+    #         ].dropna(axis=1)
+    #
+    #         cov_temp = filtered_hist_price.cov()
+    #         current_temp = current_price[[x for x in filtered_hist_price]] - np.mean(
+    #             filtered_hist_price, axis=0
+    #         )
+    #         temp = current_temp.values.dot(np.linalg.pinv(cov_temp)).dot(
+    #             current_temp.values.T
+    #         )
+    #         if temp > 0:
+    #             count += 1
+    #             if count > 2:
+    #                 turbulence_temp = temp[0][0]
+    #             else:
+    #                 # avoid large outlier because of the calculation just begins
+    #                 turbulence_temp = 0
+    #         else:
+    #             turbulence_temp = 0
+    #         turbulence_index.append(turbulence_temp)
+    #
+    #     turbulence_index = pd.DataFrame(
+    #         {"date": df_price_pivot.index, "turbulence": turbulence_index}
+    #     )
+    #     return turbulence_index
+    #
+    # def add_turbulence(self, data, time_period=252):
+    #     """
+    #     add turbulence index from a precalcualted dataframe
+    #     :param data: (df) pandas dataframe
+    #     :return: (df) pandas dataframe
+    #     """
+    #     df = data.copy()
+    #     turbulence_index = self.calculate_turbulence(df, time_period=time_period)
+    #     df = df.merge(turbulence_index, on="date")
+    #     df = df.sort_values(["date", "tic"]).reset_index(drop=True)
+    #     return df
 
     def df_to_array(self, df, tech_indicator_list, if_vix):
         df = df.copy()
