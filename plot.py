b29f7ff18d94810356be5d83d29890b17938265d
Ming Zhu <zhumingpassional@gmail.com>
2022-03-20 17:04:22 +0800

    revise data processor

--- plot.py
+++ plot.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import pyfolio
 from finrl import config
-from finrl.neo_finrl.preprocessor.yahoodownloader import YahooDownloader
+from finrl.finrl_meta.preprocessor.yahoodownloader import YahooDownloader
 from pyfolio import timeseries
 
 
