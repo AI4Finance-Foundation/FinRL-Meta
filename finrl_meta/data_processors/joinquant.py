7a11c57d4d43a28e17953c807e8c5c9678d05418
Ming Zhu <zhumingpassional@gmail.com>
2022-03-23 22:18:20 +0800

    rename processor

--- finrl_meta/data_processors/joinquant.py
+++ finrl_meta/data_processors/joinquant.py
@@ -8,7 +8,7 @@ import numpy as np
 
 from finrl_meta.data_processors._base import _Base
 
-class JoinquantProcessor(_Base):
+class Joinquant(_Base):
     def __init__(self, data_source: str, start_date: str, end_date: str, time_interval: str, **kwargs):
         super().__init__(data_source, start_date, end_date, time_interval, **kwargs)
         if 'username' in kwargs.keys() and 'password' in kwargs.keys():
--- finrl_meta/data_processors/ricequant.py
+++ finrl_meta/data_processors/ricequant.py
@@ -5,7 +5,7 @@ import rqdatac as ricequant
 from finrl_meta.data_processors._base import _Base
 
 
-class RicequantProcessor(_Base):
+class Ricequant(_Base):
     def __init__(self, data_source: str, start_date: str, end_date: str, time_interval: str, **kwargs):
         super().__init__(data_source, start_date, end_date, time_interval, **kwargs)
         if kwargs['username'] is None or kwargs['password'] is None:
