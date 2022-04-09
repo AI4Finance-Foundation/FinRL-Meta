48ec3d53a2963b34e268c01d2e5cf9bd07596f56
Ming Zhu <zhumingpassional@gmail.com>
2022-04-09 17:26:16 +0800

    add china a share, revise

--- tutorials_python/China_A_share_market.py
+++ tutorials_python/China_A_share_market.py
@@ -10,6 +10,7 @@ from IPython import display
 display.set_matplotlib_formats("svg")
 
 from finrl_meta import config
+from finrl_meta.data_processor import DataProcessor
 from main import check_and_make_directories
 from finrl_meta.data_processors.tushare import Tushare, ReturnPlotter
 from finrl_meta.env_stock_trading.env_stocktrading_China_A_shares import StockTradingEnv
@@ -75,7 +76,7 @@ check_and_make_directories([DATA_SAVE_DIR, TRAINED_MODEL_DIR, TENSORBOARD_LOG_DI
 
 # %%
 
-ticket_list = ['600000.SH', '600009.SH', '600016.SH', '600028.SH', '600030.SH',
+ticker_list = ['600000.SH', '600009.SH', '600016.SH', '600028.SH', '600030.SH',
                '600031.SH', '600036.SH', '600050.SH', '600104.SH', '600196.SH',
                '600276.SH', '600309.SH', '600519.SH', '600547.SH', '600570.SH']
 
@@ -84,50 +85,48 @@ TRAIN_END_DATE= '2019-08-01'
 TRADE_START_DATE = '2019-08-01'
 TRADE_END_DATE = '2021-01-03'
 
-token = '27080ec403c0218f96f388bca1b1d85329d563c91a43672239619ef5'
 
-# %%
+
+
+TIME_INTERVAL = "1d"
+kwargs = {}
+kwargs['token'] = '27080ec403c0218f96f388bca1b1d85329d563c91a43672239619ef5'
+p = DataProcessor(data_source='tushare', start_date=TRADE_START_DATE, end_date=TRADE_END_DATE, time_interval=TIME_INTERVAL, **kwargs)
+
+
 
 # download and clean
-ts_processor = Tushare(data_source="tushare",
-                       start_date=TRAIN_START_DATE,
-                       end_date=TRADE_END_DATE,
-                       time_interval="1d",
-                       token=token)
-ts_processor.download_data(ticker_list=ticket_list)
+p.download_data(ticker_list=ticker_list)
 
-# %%
 
-ts_processor.clean_data()
-ts_processor.dataframe
+p.clean_data()
+
 
 # %%
 
 # add_technical_indicator
-ts_processor.add_technical_indicator(config.INDICATORS)
-ts_processor.clean_data()
-ts_processor.dataframe
+p.add_technical_indicator(config.INDICATORS)
+p.clean_data()
+print(f"p.dataframe: {p.dataframe}")
 
 # %% md
 
 ### Split traning dataset
 
-# %%
-
-train = ts_processor.data_split(ts_processor.dataframe, TRAIN_START_DATE, train_stop_date)
-len(train.tic.unique())
+train = p.data_split(p.dataframe, TRAIN_START_DATE, TRAIN_END_DATE)
+print(f"len(train.tic.unique()): {len(train.tic.unique())}")
 
 # %%
 
-train.tic.unique()
+print(f"train.tic.unique(): {train.tic.unique()}")
 
 # %%
 
-train.head()
+print(f"train.head(): {train.head()}")
 
 # %%
 
-train.shape
+print(f"train.shape: {train.shape}")
 
 # %%
 
@@ -165,7 +164,7 @@ e_train_gym = StockTradingEnv(df=train, **env_kwargs)
 # %%
 
 env_train, _ = e_train_gym.get_sb_env()
-print(type(env_train))
+print(f"print(type(env_train)): {print(type(env_train))}")
 
 # %%
 
@@ -206,7 +205,7 @@ trained_a2c = agent.train_model(model=model_a2c,
 
 # %%
 
-trade = ts_processor.data_split(ts_processor.dataframe, TRADE_START_DATE, TRADE_END_DATE)
+trade = p.data_split(p.dataframe, TRADE_START_DATE, TRADE_END_DATE)
 env_kwargs = {
     "stock_dim": stock_dimension,
     "hmax": 1000,
@@ -245,8 +244,6 @@ plotter = ReturnPlotter(df_account_value, trade, TRADE_START_DATE, TRADE_END_DAT
 
 # %%
 
-% matplotlib
-inline
 plotter.plot()
 
 # %%
@@ -294,14 +291,4 @@ perf_stats_all = perf_func(returns=daily_return_base,
 print("==============Baseline Strategy Stats===========")
 perf_stats_all
 
-# %%
 
-# with pyfolio.plotting.plotting_context(font_scale=1.1):
-#         pyfolio.create_full_tear_sheet(returns = daily_return,
-#                                        benchmark_rets = daily_return_base, set_context=False)
-
-# %% md
-
-### Authors
-github
-username: oliverwang15, eitin - infant
