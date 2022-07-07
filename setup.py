f7a82aa4855be2e06b9b3c1b34d9f0cc587b4c83
eyast <eyast@microsoft.com>
2022-07-07 10:16:26 +1000

    Initial test case for YahooDownloader

--- setup.py
+++ setup.py
@@ -1,25 +1,32 @@
-from setuptools import setup
+from setuptools import find_packages, setup
+
+REQUIRES = []
+try:
+    with open("requirements.txt", "rb") as f:
+        REQUIRES = [line.strip() for line in f.read().decode("utf-8").split("\n")]
+        REQUIRES = [line for line in REQUIRES if "#" not in line]
+except FileNotFoundError as myEx:
+    raise Exception(myEx)
 
 setup(
     name="finrl_meta",
-    version="0.3.0",
+    version="0.3.1",
     author="Xiaoyang Liu, Jingyang Rui, Hongyang Yang",
     author_email="hy2500@columbia.edu",
     url="https://github.com/AI4Finance-Foundation/FinRL-Meta",
     license="MIT",
-    # dependency_links=['git+https://github.com/quantopian/pyfolio.git#egg=pyfolio-0.9.2'],
-    # install_requires=REQUIRES,
+    install_requires=REQUIRES,
     description="FinRL­-Meta: A Universe of ­ Market En­vironments for Data­-Driven Financial Reinforcement Learning",
+    packages=find_packages(),
     long_description="FinRL­-Meta: A Universe of Near Real­ Market En­vironments for Data­-Driven Financial Reinforcement Learning",
     classifiers=[
-        # Trove classifiers
-        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
