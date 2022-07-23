482ed3a574eeb8b83ad5c7f87019fa8c143d4015
Ming Zhu <zhumingpassional@gmail.com>
2022-07-23 12:08:29 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -9,6 +9,8 @@ from finrl import config
 from finrl.meta.preprocessor.yahoodownloader import YahooDownloader
 from pyfolio import timeseries
 
+from finrl.config import TRAIN_START_DATE
+
 
 def get_daily_return(df, value_col_name="account_value"):
     df = deepcopy(df)
