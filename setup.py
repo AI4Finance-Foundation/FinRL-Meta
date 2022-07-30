3dfdf9e148e8c52d988b16dffe2405f91d179d65
Bruce Yang <hongyang.yang@columbia.edu>
2022-07-30 23:30:31 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except FileNotFoundError as myEx:
 
 setup(
     name="meta",
-    version="0.3.1",
+    version="0.3.5",
     author="Xiaoyang Liu, Ming Zhu, Jingyang Rui, Hongyang Yang",
     author_email="hy2500@columbia.edu",
     url="https://github.com/AI4Finance-Foundation/FinRL-Meta",
