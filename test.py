39078e00f72a6359ff152b88d146ee711dface22
Mohammad Momayezi <47456155+mhdmyz@users.noreply.github.com>
2022-09-19 06:39:04 +0430

    Update test.py (#231)
    * Update test.py
    
    I highly suggest for simplifying and easier debugging and better readability and understandability of the codes, the environment creation gets it's own function and gets separated from the train and test functions.
    One of the prominent advantages of FinRL-Meta is it's modularity and having different independent layers, which the proposed change helps going further in this direction.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- test.py
+++ test.py
@@ -2,56 +2,19 @@
 from agents.elegantrl_models import DRLAgent as DRLAgent_erl
 from agents.rllib_models import DRLAgent as DRLAgent_rllib
 from agents.stablebaselines3_models import DRLAgent as DRLAgent_sb3
-from meta.data_processor import DataProcessor
 
 
-def test(
-    start_date,
-    end_date,
-    ticker_list,
-    data_source,
-    time_interval,
-    technical_indicator_list,
-    drl_lib,
-    env,
-    model_name,
-    if_vix=True,
-    **kwargs
-):
-
-    # process data using unified data processor
-    dp = DataProcessor(data_source, start_date, end_date, time_interval, **kwargs)
-    price_array, tech_array, turbulence_array = dp.run(
-        ticker_list, technical_indicator_list, if_vix
-    )
-    data_config = {
-        "price_array": price_array,
-        "tech_array": tech_array,
-        "turbulence_array": turbulence_array,
-    }
-
-    # build environment using processed data
-    env_instance = env(config=data_config)
-
-    env_config = {
-        "price_array": price_array,
-        "tech_array": tech_array,
-        "turbulence_array": turbulence_array,
-        "if_train": False,
-    }
-    env_instance = env(config=env_config)
-
-    # elegantrl needs state dim, action dim and net dim
+def test(drl_lib, env, model_name, **kwargs):
+    # load elegantrl needs state dim, action dim and net dim
     net_dimension = kwargs.get("net_dimension", 2**7)
     cwd = kwargs.get("cwd", "./" + str(model_name))
-    print("price_array: ", len(price_array))
 
     if drl_lib == "elegantrl":
         episode_total_assets = DRLAgent_erl.DRL_prediction(
             model_name=model_name,
             cwd=cwd,
             net_dimension=net_dimension,
-            environment=env_instance,
+            environment=env,
         )
 
         return episode_total_assets
@@ -61,9 +24,6 @@ def test(
         episode_total_assets = DRLAgent_rllib.DRL_prediction(
             model_name=model_name,
             env=env,
-            price_array=price_array,
-            tech_array=tech_array,
-            turbulence_array=turbulence_array,
             agent_path=cwd,
         )
 
@@ -71,9 +31,10 @@ def test(
 
     elif drl_lib == "stable_baselines3":
         episode_total_assets = DRLAgent_sb3.DRL_prediction_load_from_file(
-            model_name=model_name, environment=env_instance, cwd=cwd
+            model_name=model_name, environment=env, cwd=cwd
         )
 
         return episode_total_assets
+
     else:
         raise ValueError("DRL library input is NOT supported. Please check.")
