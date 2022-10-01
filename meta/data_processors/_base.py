f63ba1604611a92979734b74ee7f79d08e52d391
Ming Zhu <zhumingpassional@gmail.com>
2022-10-01 09:35:40 +0800

    revise check_date

--- meta/data_processors/_base.py
+++ meta/data_processors/_base.py
@@ -542,8 +542,8 @@ def calc_time_zone(
         raise ValueError("Time zone is wrong.")
     return time_zone
 
-def check_time_interval_date(d: str) -> bool:
-    assert len(d) == 10, 'Please check the length of date and use the correct time_interval like 2020-01-01.'
+def check_date(d: str) -> bool:
+    assert len(d) == 10, 'Please check the length of date and use the correct date like 2020-01-01.'
     indices = [0, 1, 2, 3, 5, 6, 8, 9]
     correct = True
     for i in indices:
@@ -551,7 +551,7 @@ def check_time_interval_date(d: str) -> bool:
             correct = False
             break
     if not correct:
-        raise ValueError('Please use the correct time_interval like 2020-01-01.')
+        raise ValueError('Please use the correct date like 2020-01-01.')
     return correct
 
 
