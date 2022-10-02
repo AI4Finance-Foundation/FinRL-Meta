334a90951ac6fb6d345f1994e67327f2cbba2c76
Ming Zhu <zhumingpassional@gmail.com>
2022-10-02 16:49:41 +0800

    add download data for Alpha Vantage

--- meta/data_processors/alphavantage.py
+++ meta/data_processors/alphavantage.py
@@ -2,39 +2,77 @@ import requests
 import json
 import pandas as pd
 import datetime
-# replace the "demo" apikey below with your own key from https://www.alphavantage.co/support/#api-key
-url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=demo'
-r = requests.get(url)
-data = r.json()
-print(data)
+from typing import List
+from meta.data_processors._base import _Base
+from meta.data_processors._base import calc_time_zone
+from meta.config import (
+    TIME_ZONE_SHANGHAI,
+    TIME_ZONE_USEASTERN,
+    TIME_ZONE_PARIS,
+    TIME_ZONE_BERLIN,
+    TIME_ZONE_JAKARTA,
+    TIME_ZONE_SELFDEFINED,
+    USE_TIME_ZONE_SELFDEFINED,
+    BINANCE_BASE_URL,
+)
+
+def transfer_date(d):
+    date = str(d.year)
+    date += '-'
+    if len(str(d.month)) == 1:
+        date += '0' + str(d.month)
+    else:
+        date += d.month
+    date += '-'
+    if len(str(d.day)) == 1:
+        date += '0' + str(d.day)
+    else:
+        date += str(d.day)
+    return date
+
+class Alphavantage(_Base):
+    def __init__(
+        self,
+        data_source: str,
+        start_date: str,
+        end_date: str,
+        time_interval: str,
+        **kwargs
+    ):
+        super().__init__(data_source, start_date, end_date, time_interval, **kwargs)
+
+        assert time_interval == '1d', 'please set the time_interval 1d'
+
+    # ["1d"]
+    def download_data(self, ticker_list: List[str]):
+        self.time_zone = calc_time_zone(
+            ticker_list, TIME_ZONE_SELFDEFINED, USE_TIME_ZONE_SELFDEFINED
+        )
+        self.dataframe = pd.DataFrame()
+        for ticker in ticker_list:
+            url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + ticker + '&apikey=demo'
+            r = requests.get(url)
+            data = r.json()
+            data2 = json.dumps(data['Time Series (Daily)'])
+            # gnData = json.dumps(data["Data"]["gn"])
+            df2 = pd.read_json(data2)
+            # gnDf = pd.read_json(gnData)
+
+            df3 = pd.DataFrame(df2.values.T, columns=df2.index, index=df2.columns)
+            df3.rename(columns={'1. open': 'open',
+                                '2. high': 'high',
+                                '3. low': 'low',
+                                '4. close': 'close',
+                                '5. volume': 'volume'},
+                       inplace=True)
+            df3['tic'] = ticker
+            dates = [transfer_date(df2.index[i]) for i in range(len(df2.index))]
+            df3['date'] = dates
+            self.dataframe = pd.concat([self.dataframe, df3])
+        self.dataframe = self.dataframe.sort_values(by=["date", "tic"]).reset_index(
+            drop=True
+        )
 
 
-data['Time Series (Daily)']['2022-09-30']
-'1. open'
-'2. high'
-'3. low'
-'4. close'
-'5. volume'
 
-data['Meta Data']
-'2. Symbol'
-'5. Time Zone'
-data1 = json.dumps(data['Time Series (Daily)'])
-# gnData = json.dumps(data["Data"]["gn"])
-df1 = pd.read_json(data1)
-# gnDf = pd.read_json(gnData)
-
-df2 = pd.DataFrame(df1.values.T, columns=df1.index, index=df1.columns)
-df2.rename(columns={'1. open': 'open',
-                    '2. high': 'high',
-                    '3. low': 'low',
-                    '4. close': 'close',
-                    '5. volume': 'volume'},
-            inplace=True)
-df2['tic'] = 'IBM'
-# hyDf = hyDf.set_index("IndexCode")
-# gnDf = gnDf.set_index("IndexCode")
-dates = list(df2.index)
-# 合并数据
-print(df2)
 
