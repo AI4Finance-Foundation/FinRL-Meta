9da206c53fca26b20d42b58ba34e9e1d4471660c
Caio Souza <csbc326@gmail.com>
2023-05-12 12:49:07 -0300

    Add class documentation

--- meta/env_portfolio_optimization/env_portfolio_optimization.py
+++ meta/env_portfolio_optimization/env_portfolio_optimization.py
@@ -25,8 +25,39 @@ except ModuleNotFoundError:
 class PortfolioOptimizationEnv(gym.Env):
     """A portfolio allocantion environment for OpenAI gym.
 
-    TO-DO: 
-        Document this part
+    This environment simulates the interactions between an agent and the financial market
+    based on data provided by a dataframe. The dataframe contains the time series of 
+    features defined by the user (such as closing, high and low prices) and must have
+    a time and a tic column with a list of datetimes and ticker symbols respectively.
+    An example of dataframe is shown below::
+
+            date        high            low             close           tic	  
+        0   2020-12-23  0.157414        0.127420        0.136394        ADA-USD
+        1   2020-12-23  34.381519       30.074295       31.097898       BNB-USD
+        2   2020-12-23  24024.490234    22802.646484    23241.345703    BTC-USD
+        3   2020-12-23  0.004735        0.003640        0.003768        DOGE-USD
+        4   2020-12-23  637.122803      560.364258      583.714600      ETH-USD
+        ... ...         ...             ...             ...             ...
+    
+    Based on this dataframe, the environment will create an observation space that can
+    be a Dict or a Box. The Box observation space is a three-dimensional array of shape
+    (f, n, t), where f is the number of features, n is the number of stocks in the 
+    portfolio and t is the user-defined time window. If the environment is created with
+    the parameter return_last_action set to True, the observation space is a Dict with
+    the following keys::
+
+        {
+        "state": three-dimensional Box (f, n, t) representing the time series,
+        "last_action": one-dimensional Box (n+1,) representing the portfolio weights
+        }
+
+    Note that the action space of this environment is an one-dimensional Box with size
+    n + 1 because the portfolio weights must contains the weights related to all the
+    stocks in the portfolio and to the remaining cash.
+
+    Attributes:
+        action_space: Action space.
+        observation_space: Observation space.
     """
     metadata = {"render.modes": ["human"]}
 
