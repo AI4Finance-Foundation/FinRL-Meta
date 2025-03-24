0f038500c340bb633843b6f9c5f6f57c9bf8d53e
zhumingpassional <zhumingpassional@gmail.com>
2025-03-24 14:59:38 +0800

    change calendar

--- meta/data_processors/wrds.py
+++ meta/data_processors/wrds.py
@@ -7,10 +7,10 @@ import pytz
 import wrds
 
 try:
-    import exchange_calendars as tc
+    import pandas_market_calendars as tc
 except:
     print(
-        "Cannot import exchange_calendars.",
+        "Cannot import pandas_market_calendars.",
         "If you are using python>=3.7, please install it.",
     )
     import trading_calendars as tc
