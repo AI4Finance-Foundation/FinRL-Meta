b45c6a513b216755285b03439507a0217fa248fe
Ming Zhu <zhumingpassional@gmail.com>
2025-03-26 10:25:47 +0800

    revise get_trading_days

--- meta/data_processors/alpaca.py
+++ meta/data_processors/alpaca.py
@@ -332,9 +332,11 @@ class Alpaca(_Base):
 
     def get_trading_days(self, start, end):
         nyse = tc.get_calendar("NYSE")
-        df = nyse.sessions_in_range(
-            pd.Timestamp(start, tz=pytz.UTC), pd.Timestamp(end, tz=pytz.UTC)
-        )
+        # df = nyse.sessions_in_range(
+        #     pd.Timestamp(start, tz=pytz.UTC), pd.Timestamp(end, tz=pytz.UTC)
+        # )
+        df = nyse.date_range_htf("1D", pd.Timestamp(start), pd.Timestamp(end))
+
         return [str(day)[:10] for day in df]
 
     def fetch_latest_data(
