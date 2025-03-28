a077130989460fda78c91eb74fdf64da2a8a9502
zhumingpassional <zhumingpassional@gmail.com>
2025-03-28 18:10:47 +0800

    reconstruct dataprocessor

--- unit_tests/test_baostock.py
+++ unit_tests/test_baostock.py
@@ -1,7 +1,7 @@
 import pytest
 
 from meta.data_processor import DataProcessor
-
+from meta.data_processors._base import DataSource
 
 def test_baostock():
     TRADE_START_DATE = "2020-09-01"
@@ -19,7 +19,7 @@ def test_baostock():
     ]
     kwargs = {}
     p = DataProcessor(
-        data_source="baostock",
+        data_source= DataSource.baostock,
         start_date=TRADE_START_DATE,
         end_date=TRADE_END_DATE,
         time_interval=TIME_INTERVAL,
